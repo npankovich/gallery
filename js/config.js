fab8f8aa146c5ddaf3bf7f5798f2ca5aea319a4f
npankovich <66033475+npankovich@users.noreply.github.com>
2020-06-03 13:36:50 +0300

    added request to fetch data

---
+++ js/config.js
@@ -0,0 +1,1 @@
+const apiKey = "sxOGhJvdclHCCH9sM63jZ2pF1I1DTutHovVLaAlSYZA";
--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log("working!");
+const onLoad = async () => {
+    console.log("sdsdsd");
+    const url = `https://api.unsplash.com/photos?per_page=15&order_by=popular&client_id=${apiKey}`;
+
+    let response = await fetch(url);
+
+    if (response.ok) {
+        let data = await response.json();
+        console.log(data);
+    } else {
+        alert("Error: " + response.status);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", onLoad);
